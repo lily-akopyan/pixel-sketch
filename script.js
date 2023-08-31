4a07be7dc385647923d58f49e496181551fe18c3
lily-akopyan <lily.akopyan@gmail.com>
2023-08-31 23:23:12 +0200

    fix grid

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let grid = document.querySelector('.container');
 let rows = Number(prompt("number of rows")); //prompt for testing
 let s = Math.pow(rows, 2);
-let size = 500 / rows;
+grid.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+grid.style.gridTemplateRows = `repeat(${rows}, 1fr)` ;
 
 for (let i = 0; i < s; i++) {
     const div = document.createElement('div');
-    div.style.width = size + 'px';
-    div.style.height = size + 'px';
     div.classList.add('square');
     grid.appendChild(div);
 }
