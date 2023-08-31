0acc1fce2faf90cac7cbd0902be576e6247014e6
lily-akopyan <lily.akopyan@gmail.com>
2023-09-01 00:36:42 +0200

    add grid size display

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ let rows = Number(prompt("number of rows")); //prompt for testing
 let s = Math.pow(rows, 2);
 grid.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
 grid.style.gridTemplateRows = `repeat(${rows}, 1fr)` ;
-
+let squares = document.querySelectorAll('.square');
 let resetbtn = document.getElementById('resetbtn');
+let gridsize = document.getElementById('gridsize');
+gridsize.innerText = `${rows}x${rows}`
 resetbtn.addEventListener('click', reset);
+
+
 for (let i = 0; i < s; i++) {
     const div = document.createElement('div');
     div.classList.add('square');
     grid.appendChild(div);
 }
 
-let squares = document.querySelectorAll('.square');
 squares.forEach((square) => {
     square.addEventListener("mouseover", () => {
         square.style.backgroundColor = "black";
