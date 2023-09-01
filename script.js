c665f29df74ef4097716790cafe0f843a0448e74
lily-akopyan <lily.akopyan@gmail.com>
2023-09-01 16:12:32 +0200

    add init grid && createGrid func

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 let grid = document.querySelector('.container');
-let rows = Number(prompt("number of rows")); //prompt for testing
-let s = Math.pow(rows, 2);
-grid.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
-grid.style.gridTemplateRows = `repeat(${rows}, 1fr)` ;
+// let rows = Number(prompt("number of rows")); //prompt for testing
+// let s = Math.pow(rows, 2);
 let squares = document.querySelectorAll('.square');
 let resetbtn = document.getElementById('resetbtn');
 let gridsize = document.getElementById('gridsize');
-gridsize.innerText = `${rows}x${rows}`
+createGrid(16);
 resetbtn.addEventListener('click', reset);
 
-
-for (let i = 0; i < s; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    grid.appendChild(div);
+function createGrid(r) {
+    for (let i = 0; i < r*r; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        grid.appendChild(div);
+    }
+    grid.style.gridTemplateColumns = `repeat(${r}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${r}, 1fr)`;
+    gridsize.innerText = `${r}x${r}`;
 }
+
+
+// for (let i = 0; i < s; i++) {
+//     const div = document.createElement('div');
+//     div.classList.add('square');
+//     grid.appendChild(div);
+// }
 
 squares.forEach((square) => {
     square.addEventListener("mouseover", () => {
@@ -22,6 +31,6 @@ squares.forEach((square) => {
     });
 });
 
-function reset(){
+function reset() {
     grid.innerHTML = '';
 }
