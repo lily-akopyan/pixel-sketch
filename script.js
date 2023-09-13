922e6e190a43a2ef27a0c5664229aeaa56d55732
lily-akopyan <lily.akopyan@gmail.com>
2023-09-13 12:13:58 +0200

    add modes && fix rainbow

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+//init values
 const STARTSIZE = 16;
 const STARTCOLOR = 'black';
+const STARTMODE = 'color'
 const grid = document.querySelector('.container');
 const resetbtn = document.getElementById('resetbtn');
 const sizedisp = document.getElementById('sizedisp');
 const sizeslider = document.getElementById('sizeslider');
 const colorpicker = document.getElementById('colorpicker');
 const eraserbtn = document.getElementById('eraserbtn');
+const rainbowbtn = document.getElementById('rainbowbtn');
+const colorbtn = document.getElementById('colorbtn');
+let rainbowarr = ['#0029d9','#00ccff','#ff1106','#3ef400','#4c0c7b','#c61ca4'];
 let color = STARTCOLOR;
+let mode = STARTMODE;
 createGrid(STARTSIZE);
-eraserbtn.addEventListener('click', eraser);
+
+//event listeners
+colorbtn.addEventListener('click', colorMode);
+eraserbtn.addEventListener('click', eraserMode);
+rainbowbtn.addEventListener('click', rainbowMode);
 
 resetbtn.addEventListener('click', () =>{
     reset();
@@ -26,12 +36,12 @@ colorpicker.addEventListener('input', ()=>{
     color = colorpicker.value;
 });
 
+//functions
 function createGrid(r) {
     for (let i = 0; i < r * r; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
         div.addEventListener("mouseover", hoverColor);
-        div.addEventListener("touchmove", hoverColor);
         grid.appendChild(div);
     }
     grid.style.gridTemplateColumns = `repeat(${r}, 1fr)`;
@@ -40,11 +50,29 @@ function createGrid(r) {
 }
 
 function hoverColor() {
-    this.style.backgroundColor = `${color}`;
+    switch(mode){
+        case 'color':
+            this.style.backgroundColor = `${color}`;
+            break;
+        case 'rainbow':
+            this.style.backgroundColor = `${rainbowarr[Math.floor(Math.random() * rainbowarr.length)]}`;
+            break;
+        case 'eraser':
+            this.style.backgroundColor = 'white';
+            break;
+    }
 }
 
-function eraser() {
-    color = 'white';
+function colorMode(){
+    mode = 'color';
+}
+
+function eraserMode() {
+    mode = 'eraser';
+}
+
+function rainbowMode(){
+    mode = 'rainbow';
 }
 
 function reset() {
